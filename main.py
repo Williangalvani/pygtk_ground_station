07e5bc22fc192199b3d945cb519516291c8625cf
will <wiliangalvani@gmail.com>
2013-12-18 23:34:35 -0200

    added required libs

--- main.py
+++ main.py
@@ -17,11 +17,15 @@ class BaseStation():
         self.window = self.builder.get_object("window1")
         self.compass = self.builder.get_object("compass")
         self.horizon = self.builder.get_object("horizon")
-
+        self.log = self.builder.get_object("log")
+        self.log.set_editable(False)
+        self.log.set_wrap_mode(gtk.WRAP_WORD)
+        self.logbuffer = self.log.get_buffer()
+        """@type : gtk.TextBuffer"""
         self.compass_angle = 0
         self.roll_angle=0
         self.pitch_angle=0
-        self.image_names = ['compass.png', 'horizon.png', 'battery.png'] # horizon cant be first, regular size is based on compass' size
+        self.image_names = ['compass.png', 'horizon2.png', 'battery.png'] # horizon cant be first, regular size is based on compass' size
         self.mask = Image.open('mask.png').convert('L')
         self.desired_size = None
 
@@ -30,6 +34,7 @@ class BaseStation():
 
         self.update()
         self.window.show_all()
+        self.window.connect("destroy", gtk.main_quit)
         gtk.main()
 
     def loadImages(self):
@@ -39,8 +44,8 @@ class BaseStation():
             self.desired_size = self.images[self.image_names[0]].getbbox()[2:]
     def updateImages(self):
         #print dir(self.compass)
-        self.compass.set_from_pixbuf(self.get_rotated_image('compass.png',self.compass_angle))
-        self.horizon.set_from_pixbuf(self.get_rotated_and_shifted_horizon_image(self.roll_angle,self.pitch_angle))
+        self.compass.set_from_pixbuf(self.get_rotated_image('compass.png', self.compass_angle))
+        self.horizon.set_from_pixbuf(self.get_rotated_and_shifted_horizon_image(self.roll_angle, self.pitch_angle))
 
     def get_rotated_image(self,image,angle):
         src_im = self.images[image]
@@ -48,7 +53,7 @@ class BaseStation():
         dst_im = Image.new("RGBA",size, "white" )
         im = src_im.convert('RGBA')
         rot = im.rotate( angle, expand=0, )#.resize(size)
-        dst_im.paste( rot, (0, 0), rot )
+        dst_im.paste( rot, (0, 0), rot)
         dst_im.save("test.png")
         arr = numpy.array(self.apply_circular_mask(dst_im))
         return gtk.gdk.pixbuf_new_from_array(arr, gtk.gdk.COLORSPACE_RGB, 8)
@@ -60,13 +65,13 @@ class BaseStation():
         return output
 
     def get_rotated_and_shifted_horizon_image(self, angle, shift):
-        src_im = self.images['horizon.png']
+        src_im = self.images['horizon2.png']
         size = src_im.getbbox()[2:]
         dst_im = Image.new("RGBA",size, "blue" )
         im = src_im.convert('RGBA')
         rot = im.rotate( angle, expand=0, )
         angle = radians(angle)
-        dst_im.paste( rot, (int(shift*sin(angle)), int(shift*cos(angle))), rot )
+        dst_im.paste( rot, (int(shift*sin(angle)), int(shift*cos(angle))), rot)
         desired_height = self.desired_size[1]
         desired_width = self.desired_size[0]
         dy = desired_height/2
@@ -79,10 +84,16 @@ class BaseStation():
         #print dir(self.compass)
         return gtk.gdk.pixbuf_new_from_array(arr, gtk.gdk.COLORSPACE_RGB, 8)
 
+    def log_text(self,string):
+        self.logbuffer.insert(self.logbuffer.get_end_iter(),string)
+
     def update(self):
         self.compass_angle += 2
         self.roll_angle += random.randrange(-2,2);
-        self.pitch_angle += random.randrange(-2,2)
+        self.pitch_angle += random.randrange(-1,1)
+        print type(self.log)
+        self.log_text("o haay\n")
+        #print dir(self.log)
         #print self.pitch_angle,self.roll_angle
 
 
